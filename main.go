5733eaaeb7d5605147d71327fa642208ef23741f
Mantai59 <mmantaibusiness@gmail.com>
2023-04-19 09:26:51 +0200

    feat: database calls for testdata

--- main.go
+++ main.go
@@ -10,15 +10,21 @@ import (
 
 func dbConn(maxConnections int) *sql.DB {
 	db, err := sql.Open("mysql", "root:@/userdata")
-
-	if err != nil {
-		panic(err)
-	}
-
+	dbError(err)
 	db.SetMaxOpenConns(maxConnections)
 	db.SetMaxIdleConns(maxConnections)
 	
 	return db
+}
+
+func dbClose(db *sql.DB) {
+	db.Close()
+}
+
+func dbError(err error) {
+	if(err != nil) {
+		panic(err)
+	}
 }
 
 
@@ -31,17 +37,29 @@ type user struct {
 	email string `json:"email"`
 }
 
-var users = []user{
-	{rowid: 1, name: "testuser", pw:"efjpokewfnewpofewf", email:"test@test.com"},
-}
+// var users = []user{
+// 	{rowid: 1, name: "testuser", pw:"efjpokewfnewpofewf", email:"test@test.com"},
+// }
+
+
 
 func getUsers(c *gin.Context) {
+	db := dbConn(10)
+	defer dbClose(db)
+	smt, err := db.Prepare("SELECT * FROM users")
+	dbError(err)
+	defer smt.Close()
+	users, err := smt.Exec()
 	c.IndentedJSON(http.StatusOK, users)
 }
 
+
+//main function
 func main() {
 	router := gin.Default()
+	//define routes
 	router.GET("/user", getUsers)
 	fmt.Println("Starting Rest Api on port 8080")
+	//start server
 	router.Run("127.0.0.1:8080")
 }
